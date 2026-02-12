14a23a21b958f3c3fce73548e8a3081dfaae2b35
udod45 <ujrathod1204@gmail.com>
2026-02-12 12:32:38 +0530

    coffee completed

--- src/07-coffee-shop.js
+++ src/07-coffee-shop.js
@@ -32,4 +32,42 @@
  */
 export function calculateCoffeePrice(size, type, extras = {}) {
   // Your code here
+  let total = 0;
+  const { whippedCream = false, extraShot = false } = extras;
+ if (size !== "small" && size !== "medium" && size !== "large"){
+  return -1;
+ }
+ if (type !== "regular" && type !== "latte" && type !== "cappuccino" && type !== "mocha"){
+  return -1;
+ }
+
+if (size === "small"){
+  total+=3.00;
+}
+else if (size === "medium"){
+  total+=4.00;
+}
+else if (size === "large"){
+  total+=5.00;
+}
+
+if (type === "regular"){
+  total+=0.00;
+}
+else if (type === "latte"){
+  total+=1.00;
+}
+else if (type === "cappuccino"){
+  total+=1.50;
+}
+else if (type === "mocha"){
+  total+=2.00;
+}
+ if (whippedCream === true ){
+  total+=0.50;
+ }
+ if (extraShot === true){
+  total+=0.75;
+ }
+return total;
 }
